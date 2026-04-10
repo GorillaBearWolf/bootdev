9e56bea8192dd550acead9c15424043b238af54b
Matthew Refsnider <mrefsnider@icloud.com>
2026-04-10 18:50:23 -0400

    update: add course folders

---
+++ bookbot/README.md
@@ -0,0 +1,3 @@
+# Boot.dev Ch. 3 Project
+
+BookBot is a CLI tool that generates a report from a text file.
---
+++ bookbot/main.py
@@ -0,0 +1,24 @@
+﻿BOOK_PATH = "books/frankenstein.txt"
+
+with open(BOOK_PATH) as file:
+    book_text = file.read()
+
+words = book_text.split()
+
+lowered = book_text.lower()
+
+letter_dict = {}
+
+for i in lowered:
+    if i in letter_dict:
+        letter_dict[i] += 1
+    else:
+        letter_dict[i] = 1
+
+# Create and print formatted report
+print(f"--- Report of {BOOK_PATH} ---", "\n")
+print(len(words), "words found in the text", "\n")
+for i in sorted(letter_dict, key=letter_dict.get, reverse=True):
+    if i.isalpha():
+        print(f"The '{i}' character was found {letter_dict[i]} times")
+print("\n", "--- End of report ---")
